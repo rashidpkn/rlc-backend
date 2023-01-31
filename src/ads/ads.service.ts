09850cc9b62ebe4f365268de9422d28c12dd169a
Muhammed Rashid N <muhammed916rashid@gmail.com>
2023-01-31 17:01:25 +0530

    Somthing is added

--- src/ads/ads.service.ts
+++ src/ads/ads.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { create } from 'domain';
 import { Ads } from 'src/database/model/ads.entity';
 
 @Injectable()
 export class AdsService {
     create(ads: JSON, username: string, email: string) {
         try {
-            Ads.create({ ...ads, username, email })
+            Ads.create({ ...ads, username, email }).catch(error=>{
+                console.log(error.message)
+                return ({
+                    status: false,
+                    reason: error.message
+                })
+            })
+           
             return ({
                 status: true,
                 reason: 'Ads is created'
             })
         } catch (error) {
+            console.log(error.message)
             return ({
                 status: false,
                 reason: error.message
